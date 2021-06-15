dfe6b45c59092da4b0cefa7a06e2c5364b6152be
Amoong <muyoung.jeong@streami.co>
2021-06-15 15:35:19 +0900

    css-module

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div className="About">
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore odit dolor corrupti, magnam autem ipsum consequatur aliquid officia mollitia saepe.
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import styles from './MainPage.module.scss'
+
+function MainPage() {
+  return (<div className={styles.MainPage}>
+    <h1>Main Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, earum!
+    </p>
+  </div>
+  )
+}
+
+export default MainPage
