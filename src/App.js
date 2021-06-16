359f9be6c1df4e397081dfb9632197c738318b26
Amoong <moo1323@naver.com>
2021-06-16 10:26:44 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import { useState } from 'react' 
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import { useState } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
-import styles from './App.module.scss'
+import styles from "./App.module.scss";
 
-import Nav from './Nav'
-import AboutPage from './pages/AboutPage'
-import MainPage from './pages/MainPage'
+import Nav from "./Nav";
+import AboutPage from "./pages/AboutPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  const [isDarkTheme, setIsDarkTheme] = useState(true)
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
 
   return (
-    <div className={isDarkTheme ? 'dark' : 'light'}>
-      <h1 className={styles.title}>css-module</h1>
-      <button size="small" onClick={() => setIsDarkTheme(!isDarkTheme)}>{isDarkTheme ? '🌙' : '☀️'}</button>
-      <Router>
-        <Nav />
-        <Switch>
-          <Route path="/about">
-            <AboutPage/>
-          </Route>
-          <Route path="/">
-            <MainPage />
-          </Route>
-        </Switch>
-      </Router>
+    <div className={isDarkTheme ? "dark" : "light"}>
+      <div className={styles.App}>
+        <h1 className={styles.title}>css-module</h1>
+        <button size="small" onClick={() => setIsDarkTheme(!isDarkTheme)}>
+          {isDarkTheme ? "🌙" : "☀️"}
+        </button>
+        <Router>
+          <Nav />
+          <Switch>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route path="/">
+              <MainPage />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,15 @@
-import styles from './MainPage.module.scss'
+import styles from "./MainPage.module.scss";
 
 function MainPage() {
-  return (<div className={styles.MainPage}>
-    <h1>Main Page</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, earum!
-    </p>
-  </div>
-  )
+  return (
+    <div className={styles.MainPage}>
+      <h1 className={styles.title}>Main Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+        earum!
+      </p>
+    </div>
+  );
 }
 
-export default MainPage
+export default MainPage;
